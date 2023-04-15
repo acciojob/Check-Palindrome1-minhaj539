a61143796e27a3395a2a77594f731e4f5ccbb80f
minhaj539 <115364661+minhaj539@users.noreply.github.com>
2023-04-16 04:25:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 function palindrome(str){
 	let i=0,j=str.length-1;
 	while(i<j){
-		let a=str.charAt(i).toLowerCase;
-		let b=str.charAt(j).toLowerCase;
+		let a=str.charAt(i).toLowerCase();
+		let b=str.charAt(j).toLowerCase();
 		if(a!=b) return false;
 		i++;
 		j--;
